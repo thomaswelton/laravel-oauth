c8ab7966edbfcbb072d5019d3987057e889c069c
Thomas Welton <thomaswelton@me.com>
2013-08-24 17:58:32 +0100

    Initial commit from php artisan workbench

---
+++ src/Thomaswelton/LaravelOauth/LaravelOauthServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Thomaswelton\LaravelOauth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOauthServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('thomaswelton/laravel-oauth');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
